f15762dc4ec7142606e39dae30a13cb495bf926b
Giancarlo <gianacevedof@gmail.com>
2026-09-22 18:02:21 -0400

    feat(api): scaffold core rest route handlers

--- server/server.js
+++ server/server.js
@@ -1,11 +1,17 @@
 import express from "express";
 import cors from "cors";
 import pool from "./db.js";
+import accountsRoutes from "./routes/accounts.js";
+import transactionsRoutes from "./routes/transactions.js";
+import budgetsRoutes from "./routes/budgets.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/accounts", accountsRoutes);
+app.use("/api/transactions", transactionsRoutes);
+app.use("/api/budgets", budgetsRoutes);
 
 app.get("/api/health", async (req, res) => {
   try {
