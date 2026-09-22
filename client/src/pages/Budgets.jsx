2bce5478a66a9f7f876aac653ff8cc47183aad3e
Giancarlo <gianacevedof@gmail.com>
2026-09-22 18:53:30 -0400

    deleted comment lines

--- client/src/pages/Budgets.jsx
+++ client/src/pages/Budgets.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 
-// src/pages/Budgets.jsx
-
 function Budgets() {
   return (
     <div>
--- client/src/pages/Transactions.jsx
+++ client/src/pages/Transactions.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 
-// src/pages/Transactions.jsx
-
 function Transactions() {
   return (
     <div>
