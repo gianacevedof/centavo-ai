ea692765963228cc7e2ae9859ece45dce8b49248
Giancarlo <gianacevedof@gmail.com>
2026-09-22 18:02:53 -0400

    added empty budgets.jsx page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import Dashboard from "./pages/Dashboard";
 import Transactions from "./pages/Transactions";
+import Budgets from "./pages/Budgets";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 function App() {
@@ -10,12 +11,14 @@ function App() {
       <nav>
         <Link to="/">Dashboard</Link>
         <Link to="/transactions">Transactions</Link>
+        <Link to="/budgets">Budgets</Link>
       </nav>
 
       {/* routes */}
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/transactions" element={<Transactions />} />
+        <Route path="/budgets" element={<Budgets />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ client/src/pages/Budgets.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Budgets() {
+  return <div>This is the budgets page.</div>;
+}
+
+export default Budgets;
